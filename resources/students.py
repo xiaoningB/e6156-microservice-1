6704f2d3cc31b331fb56d1aa0c3d9de4e3ac2607
xiaoningB <144538938+xiaoningB@users.noreply.github.com>
2023-09-20 21:24:54 -0400

    Update students.py

--- resources/students.py
+++ resources/students.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 class StudentsResource:
@@ -7,8 +8,7 @@ class StudentsResource:
     # It is also pretty sloppy code.
     #
 
-    students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+    students_file = os.path.join(os.path.dirname(__file__), "old-students.json")
 
     def __init__(self):
         self.students = None
